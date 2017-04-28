ad63c918ef9f5186d5c93eb7a301d4315047e133
Shaun Coleman <shaunpcole@gmail.com>
2017-04-28 12:23:44 -0700

    Updated HW4

--- hw4.asm
+++ hw4.asm
@@ -1,5 +1,7 @@
 .ORIG x3000
 LEA R1, MEMORY
+LEA R0, PROMPT1
+PUTS
 LOOP:
 	LEA R0, MEMORY
         GETC ;input character 
@@ -9,8 +11,17 @@ LOOP:
         ADD R2, R2, R0 ; Use R2 to check for Newline (0x0A)
         ADD R2, R2, #-10 ; 0 if char is Newline
         BRnp LOOP ; If R2 is not Newline loop;
+ADD R1, R1, #-1
+STR R2, R1, #0 ;put this in my memory block
+LEA R0, PROMPT2
+PUTS
+GETC
+; -> Go to Sub Routine
 LEA R0, MEMORY
 PUTS
 HALT
+SALT .FILL #3
+PROMPT1 .STRINGZ "Please enter your name."
+PROMPT2 .STRINGZ "\nPress any key to continue: "
 MEMORY .BLKW #20
 .END
