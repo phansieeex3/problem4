45a5d3e723a5378b716de4d31b9bcba5f70200df
Shaun Coleman <shaunpcole@gmail.com>
2017-04-28 11:18:26 -0700

    New Changes from start of class

--- hw4.asm
+++ hw4.asm
@@ -1,9 +1,15 @@
 .ORIG x3000
 LEA R1, MEMORY
+AND R1, R1, #0
 LOOP:
 	GETC ;input character 
 	STR R0, R1, #0 ;put this in my memory block
 	ADD R1, R1, #1 ;increment the memory pointer
-	BR LOOP
+        AND R2, R2, #0
+        ADD R2, R2, R0
+        ADD R2, R2, #-10
+	BRnp LOOP
+PUTS
+HALT
 MEMORY .BLKW #20
 .END
