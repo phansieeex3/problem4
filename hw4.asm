e3bb19b87c69490724a1815444f3b053b5409fee
Shaun Coleman <shaunpcole@gmail.com>
2017-04-28 12:00:38 -0700

    Added comments

--- hw4.asm
+++ hw4.asm
@@ -6,9 +6,9 @@ LOOP:
 	STR R0, R1, #0 ;put this in my memory block
 	ADD R1, R1, #1 ;increment the memory pointer
         AND R2, R2, #0
-        ADD R2, R2, R0
-        ADD R2, R2, #-10
-        BRnp LOOP
+        ADD R2, R2, R0 ; Use R2 to check for Newline (0x0A)
+        ADD R2, R2, #-10 ; 0 if char is Newline
+        BRnp LOOP ; If R2 is not Newline loop;
 LEA R0, MEMORY
 PUTS
 HALT
